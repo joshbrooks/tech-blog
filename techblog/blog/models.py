d427ae685a09a588acf7bca14c9a7fa02b4253f5
Joshua Brooks <josh.vdbroek@gmail.com>
2019-05-30 14:06:37 +0800

    BlogPageGalleryImage

--- techblog/blog/models.py
+++ techblog/blog/models.py
@@ -1,18 +1,19 @@
-from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
-
 from django.db import models
 
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
+    content_panels = Page.content_panels + [FieldPanel("intro", classname="full")]
+
 
 class BlogPage(Page):
     date = models.DateField("Post date")
@@ -20,12 +21,26 @@ class BlogPage(Page):
     body = RichTextField(blank=True)
 
     search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
+        index.SearchField("intro"),
+        index.SearchField("body"),
     ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
-        FieldPanel('intro'),
-        FieldPanel('body', classname="full"),
+        FieldPanel("date"),
+        FieldPanel("intro"),
+        FieldPanel("body", classname="full"),
+        InlinePanel("gallery_images", label="Gallery images"),
     ]
+
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(
+        BlogPage, on_delete=models.CASCADE, related_name="gallery_images"
+    )
+    image = models.ForeignKey(
+        "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [ImageChooserPanel("image"), FieldPanel("caption")]
+
